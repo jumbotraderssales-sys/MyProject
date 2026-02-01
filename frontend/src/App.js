f496a3a02ad703dc359ee9e0367bdded0f0763b9
jumbotraderssales-sys <jumbotraders.sales@gmail.com>
2026-02-02 02:52:35 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import AccountSetup from './components/AccountSetup';
 import WithdrawalRequest from './components/WithdrawalRequest';
 import AdminWithdrawalPanel from './components/AdminWithdrawalPanel';
 import WithdrawalHistory from './components/WithdrawalHistory';
-import Navbar from './components/Navbar'; // Adjust the path
 
 import './App.css';
 
@@ -82,15 +81,6 @@ const PLANS = [
     paperMoney: "500,000"
   }
 ];
-function App() {
-  return (
-    <div className="advanced-app">
-      <Navbar />
-      {/* Rest of your app content */}
-    </div>
-  );
-}
-
 function App() {
   const [userAccount, setUserAccount] = useState({
     id: null,
